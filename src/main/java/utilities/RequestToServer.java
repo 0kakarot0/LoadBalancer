7cd584fc07e6e8d8c373971395d5bc58d21d88ee
Ahtisham <ilyasahtsham@gmail.com>
2024-06-22 17:31:16 +0500

    Update RequestToServer.java
    changes in the thread concurrency class

--- src/main/java/utilities/RequestToServer.java
+++ src/main/java/utilities/RequestToServer.java
@@ -1,29 +1,53 @@
 package utilities;
 
+import java.io.IOException;
+
 class Requests_Concurrency extends Thread {
     private Thread thread;
     private String threadName;
     private int totalRequest;
+    private String receivedRequestOnServer;
+    private StringBuilder stringBuilder;
 
-    Requests_Concurrency(String name, int totalRequest) {
+    Requests_Concurrency(String name, int totalRequest, String receivedRequestOnServer) {
         this.threadName = name;
         this.totalRequest = totalRequest;
+        this.receivedRequestOnServer = receivedRequestOnServer;
     }
+
 
     @Override
     public void run() {
         try {
-            System.out.println("Response from server: HTTP/1.1 200 OK\n" + "\n" + "Hello From Backend Server");
+//            for (int i = 0; i < totalRequest; i++) {
+            System.out.println(returnProperFormattedResponse());
+//                System.out.println("Response from server: HTTP/1.1 200 OK\n" + "Hello From Backend Server"+ "\n" );
+//            }
         } catch (Exception e) {
-            System.out.println("Invalid Request to server");
+            System.out.println("Server Not Alive " + threadName);
+        }
+    }
+
+    private String returnProperFormattedResponse() {
+        try {
+            stringBuilder = new StringBuilder();
+            stringBuilder.append("\n");
+            stringBuilder.append(receivedRequestOnServer).append("\n");
+            stringBuilder.append("************************* RESPONSE START *************************").append("\n");
+            stringBuilder.append(MyHTTPRequestToServer.returnResponseFromServer(threadName)).append("\n");
+            stringBuilder.append("************************* RESPONSE ENDS *************************").append("\n");
+            Thread.sleep(500);
+            return stringBuilder.toString();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
 
 
 public class RequestToServer {
-    public static void sendRequestToBackEndServer(String threadName, int totalRequest) {
-        Requests_Concurrency requestsConcurrency = new Requests_Concurrency(threadName, totalRequest);
+    public static void sendRequestToBackEndServer(String threadName, int totalRequest, String receivedRequestOnServer) {
+        Requests_Concurrency requestsConcurrency = new Requests_Concurrency(threadName, totalRequest, receivedRequestOnServer);
         requestsConcurrency.start();
     }
 }
